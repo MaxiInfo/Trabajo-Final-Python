48849b5cbcf4cfed02fff7acea6ef61f93b22fa4
tensa2205 <62038818+tensa2205@users.noreply.github.com>
2020-05-23 21:25:39 -0300

    Añadido top 10 malardo

--- archivos py/TopString.py
+++ archivos py/TopString.py
@@ -1,5 +1,5 @@
 def generar_string(diccionario):
-    string = 'Nombre    Puntaje     Fecha\n'
+    string = 'Nombre | Puntaje | Fecha\n'
     for clave,valor in diccionario.items():
-        string += clave + "     " + valor[0] + "    " + valor[1] + "\n"   
+        string +=  clave + "    |    " + str(valor[0]) + "     |     " + valor[1] + "\n"  
     return string
