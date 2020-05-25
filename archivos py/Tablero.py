68238a9a9db454f9ebea40552bc16bcaf5f3766e
MaxiInfo <62036075+MaxiInfo@users.noreply.github.com>
2020-05-24 21:34:49 -0300

    dfsgfghklj

--- archivos py/Tablero.py
+++ archivos py/Tablero.py
@@ -1,7 +1,8 @@
 import PySimpleGUI as sg
 from Class_tablero import Tablero
-
+from Class_Dificultad import dificultad as dif
 def game_start (window):
+    dif.establecer_dificultad(configs['dificultad'])
     pass
 
 def main(configs):
@@ -14,7 +15,10 @@ def main(configs):
             game_start(window)
         elif event is 'Posponer':
             window.FindElement('-MESSAGE-').Update('Debes comenzar el juego para podes posponerlo')
+        else:
+            window.FindElement('-MESSAGE-').Update('Comienza el juego para poder utilizar la interfaz')
     return event
 
+#configs = dict({'name':'Player','timing':20,'dificultad':'medio'})
 
 #configs = {'name':values[0],'timing':values[1],'dificultad':'facil'if values[2] else 'medio' if values[3]else 'dificil'}
