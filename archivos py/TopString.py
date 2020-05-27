168c02f8eb945e49f473a226ad7a7bb5b674dd13
MaxiInfo <62036075+MaxiInfo@users.noreply.github.com>
2020-05-27 13:44:12 -0300

    prueba delete

--- archivos py/TopString.py
+++
@@ -1,5 +0,0 @@
-def generar_string(diccionario):
-    string = 'Nombre | Puntaje | Fecha\n'
-    for clave,valor in diccionario.items():
-        string +=  clave + "    |    " + str(valor[0]) + "     |     " + valor[1] + "\n"  
-    return string
