5b6a9c098b2f352576b58efe0b6f9c332ddb3fbf
tensa2205 <62038818+tensa2205@users.noreply.github.com>
2020-05-31 00:01:57 -0300

    Cambios administrador y csv agregados
    Administrador ->- crear_tuplas
                                - calcular_puntaje
    CSV -> -tuplas_facil
                 -tuplas_medio

--- archivos py/Class_administrador.py
+++ archivos py/Class_administrador.py
@@ -10,8 +10,15 @@ class AdministradorDeJuego():
     tupla_verb = ('VAG','VBG','VAI','VAN','MD','VAS','VMG','VMI','VB','VMM','VMN','VMP','VBN','VMS','VSG','VSI','VSN','VSP','VSS')
     
 
-    def __init__(self,dif = 'facil'):
+    def __init__(self,dif = 'medio'):
         self._dificultad_actual = dif #puede cambiar pero puse medio como PREDETERMINADO
+        self._tuplas_DL = []
+        self._tuplas_TL = []
+        self._tuplas_DW = []
+        self._tuplas_TW = []
+        self._tuplas_R10 = []
+        self._tuplas_R20 = []
+        self.crear_tuplas()
         self.crear_diccionarios()
 
     def set_dificultad(self, nueva_dificultad):
@@ -48,7 +55,7 @@ class AdministradorDeJuego():
         '''
             Comprueba en el spelling y en el lexicon
         '''
-        return palabra in spelling and palabra in lexicon
+        return palabra in spelling or palabra in lexicon
 
     def __es_correcta_facil(self,palabra):
         '''
@@ -160,8 +167,75 @@ class AdministradorDeJuego():
             self.no_hay_mas(aux,letra_actual)
                 
         return fichas
+    def crear_tuplas(self):
+        ubicacion = 'tuplas_' + self._dificultad_actual + ".csv" # tuplas_dificultad.csv. Sólo para abrir
+        archivo_csv = open(ubicacion,'r',encoding= 'utf8') #abro el archivo
+        csv_reader = csv.reader(archivo_csv, delimiter = ',', quotechar = '"') 
+        next(archivo_csv) # me salteo la primer linea que sólo contiene la información de las columnas.
 
 
-objeto = AdministradorDeJuego('facil')
-print(objeto.es_correcta('lápiz'))
+        for columna in csv_reader:
+            fila_actual = int(columna[0])
+            columna_actual = int(columna[1])
+            tupla = (fila_actual,columna_actual)
+            identificador = columna[2] # DL, TL, DW, TW, R10, R20
+
+            if identificador == 'DW':
+                self._tuplas_DW.append(tupla)
+            elif identificador == 'TW':
+                self._tuplas_TW.append(tupla)
+            elif identificador == 'DL':
+                self._tuplas_DL.append(tupla)
+            elif identificador == 'TL':
+                self._tuplas_TL.append(tupla)
+            elif identificador == 'R10':
+                self._tuplas_R10.append(tupla)
+            else:
+                self._tuplas_R20.append(tupla)
+    def devolver_tuplas(self):
+        '''
+            devuelve una lista de listas con todas las tuplas
+            [0] -> Double Word
+            [1] -> Triple Word
+            [2] -> Double Letter
+            [3] -> Triple Letter
+            [4] -> Resta 10
+            [5] -> Resta 20
+        '''
+        lista_tuplas = [self._tuplas_DW,self._tuplas_TW,self._tuplas_DL,self._tuplas_TL,self._tuplas_R10,self._tuplas_R20]
+        return lista_tuplas
+    def calcular_puntaje(self,palabra,tupla):
+        puntaje = 0
+        multiplicador = 0
+
+        for i in range(len(palabra)): #de 0 hasta len(palabra) -1
+            posicion_actual = tupla[i]
+
+            if posicion_actual in self._tuplas_DW: #Double word
+                multiplicador += 2
+            elif posicion_actual in self._tuplas_TW: #Triple word
+                multiplicador += 3
+            elif posicion_actual in self._tuplas_DL: #Double letter
+                puntaje += self._diccionario_puntaje[palabra[i].upper()] * 2
+            elif posicion_actual in self._tuplas_TL: #Triple letter
+                puntaje += self._diccionario_puntaje[palabra[i].upper()] * 3
+            elif posicion_actual in self._tuplas_R10: #Restar 10
+                puntaje += self._diccionario_puntaje[palabra[i].upper()] -10
+            elif posicion_actual in self._tuplas_R20: #Restar 20
+                puntaje += self._diccionario_puntaje[palabra[i].upper()] -20
+            else: #La letra tocó una casilla no especial
+                puntaje += self._diccionario_puntaje[palabra[i].upper()]
+
+        if multiplicador != 0 and puntaje > 0: #Significa que tocó algun DoubleWord o Triple Word. Eso sí, si el puntaje es negativo, no se aumenta
+            puntaje *= multiplicador
+        return puntaje
+
+
+objeto = AdministradorDeJuego()
+tupla = ((3,0),(1,1),(12,12)) #DL - DW - R10 -> existen en el csv 
+valor = objeto.calcular_puntaje('zed',tupla) # z -> 10, e -> 4, d -> 2
+                            # 20 -8 -> el 4 se saltea por que es Double Word.
+                            #12 * 2 -> 34
+print(valor)
+
 
