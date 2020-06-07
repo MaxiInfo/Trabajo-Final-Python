7173e2fcf2577b4f38848ff6730304b762994b19
MaxiInfo <maxirauch@hotmail.com>
2020-06-07 20:41:50 -0300

    solucion de bug, casilleros quedaban bloqueados

--- archivos py/Tablero.py
+++ archivos py/Tablero.py
@@ -5,32 +5,29 @@ from Class_Jugador import jugador
 from random import randint as rand
 import time
 
-admin = AdministradorDeJuego()
-lista_tuplas = admin.devolver_tuplas()
-board = Tablero(lista_tuplas)
 teclas = ('0','1','2','3','4','5','6')
 
 #tiempo_juego = 0
 #tiempo_restante = 0
 
-def game_on (window,configs,fichas_jugador):
+def game_on (window,admin,board,configs,fichas_jugador):
     window.FindElement('-MESSAGE-').Update('Comenzo la partida')
     admin.set_dificultad(configs['dificultad'])
     board.table_on(window)
     board.update_fichas_player(window,fichas_jugador)
     pass
 
-def block_word(window,tuple_list,palabra):
+def block_word(window,tuple_list,palabra,board):
     board.mod_board(tuple_list,palabra)
     for i in tuple_list:
         window.FindElement(i).Update(disabled=True,button_color = ('black','#58F76D'))
     pass
 
-def vuelta_atras(window,player,letter,tupla,cant,escritura):
+def vuelta_atras(window,player,board,letter,tupla,cant,escritura):
     board.set_default_button(window,tupla)
     pos = player.pos_libre()
     player.set_single_ficha(letter,pos)
-    window.FindElement(str(pos)).Update(letter)
+    window.FindElement(str(pos)).Update(letter,disabled = False)
     if cant == 1:
         tupla_aux1 = (tupla[0],tupla[1]+1) 
         tupla_aux2 = (tupla[0]+1,tupla[1]) 
@@ -57,7 +54,7 @@ def vuelta_atras(window,player,letter,tupla,cant,escritura):
         window.FindElement(tupla).Update(button_color = ('black','#4E61DC'))
     pass
 
-def cambiar_fichas(window,player):
+def cambiar_fichas(window,player,admin):
     cant = 0
     tecla_select = []
     window.FindElement('-MESSAGE-').Update('seleccione hasta 3 letras para cambiar')
@@ -112,17 +109,17 @@ def ingresa_tercera(window,event,escritura,letter,next_button):
     window.FindElement(event).Update(letter,button_color = ('white','blue'))
     return next_button
 
-def fill_letters(player,window):
+def fill_letters(player,window,admin):
     list_fichas = player.get_fichas()
     for i in range(len(list_fichas)):
         if list_fichas[i] == 0:
             ficha = admin.tomar_fichas(1)
             list_fichas[i] = ficha
-            window.FindElement(str(i)).Update(ficha)
+            window.FindElement(str(i)).Update(ficha,disabled=False)
     player.set_fichas(list_fichas)
     pass
 
-def play_player (player, window):
+def play_player (player, window,admin,board):
     '''
     el modulo play_player es el encargado del manejo de la colocacion de palabras en el tablero y el manejr de la interfaz 
     que el usuario va a tener dentro del juego
@@ -157,13 +154,13 @@ def play_player (player, window):
         if event in (None, '-mainMenu-'):
             break
 #==========================================================================================================================#
-        if event == '-changeAll-':
+        if event == '-changeAll-': #and cant_letras != 0
             player.set_fichas(admin.tomar_fichas(7))
             board.update_fichas_player(window,player.get_fichas())
             continue
 #==========================================================================================================================#  
-        if event == '-change-':
-            event = cambiar_fichas(window,player)
+        if event == '-change-' and cant_letras == 0:
+            event = cambiar_fichas(window,player,admin)
             break
 #==========================================================================================================================#       
         if event in teclas:
@@ -171,20 +168,23 @@ def play_player (player, window):
             if not letter_selected:
                 # si no hay fichas seleccionadas tomo una ficha del atril, actualizo fichas del jugador
                 # update de ficha actual seleccionada
+                pos_en_atril = event
                 letter = get_ficha(int(event))
                 window.FindElement(event).Update('')
                 window.FindElement('-LetterSelected-').Update(letter)
                 letter_selected = True
             else:
                 # devoludion de ficha seleccionada
-                if player.ficha_pos(int(event)) == 0:
+                if event == pos_en_atril:
                     # si se quiere devolver a la misma posicion donde se tomo
+                    pos_en_atril = None
                     window.FindElement(event).Update(letter)
                     set_ficha(letter,int(event))
                     window.FindElement('-LetterSelected-').Update('')
                     letter_selected = False
                 else:
                     # si se quiere intercambiar de ficha actual con otra en el atril
+                    pos_en_atril = event
                     window.FindElement(event).Update(letter)
                     letter = player.change_single_ficha(letter,int(event))
                     window.FindElement('-LetterSelected-').Update(letter)
@@ -200,6 +200,7 @@ def play_player (player, window):
                 cant_letras += 1
                 tuple_list = [event]
                 next1,next2 = ingresa_primera(window,event,letter)
+                window.FindElement(pos_en_atril).Update(disabled=True)
                 window.FindElement('-LetterSelected-').Update('')
             elif cant_letras == 1:
                 #si ya esta ingresada la primera letra
@@ -217,7 +218,8 @@ def play_player (player, window):
                     if 15 not in next_button:
                         window.FindElement(next_button).Update(button_color = ('black','#4E61DC'))
                     escritura = 'ArribaAbajo'
-                    window.FindElement('-LetterSelected-').Update('')                    
+                    window.FindElement(pos_en_atril).Update(disabled=True)
+                    window.FindElement('-LetterSelected-').Update('')
                 elif (event == next2):
                     #si se ingreso de Izquierda a Derecha
                     palabra += letter
@@ -232,18 +234,21 @@ def play_player (player, window):
                     if 15 not in next_button:
                         window.FindElement(next_button).Update(button_color = ('black','#4E61DC'))
                     escritura = 'IzqDer'
+                    window.FindElement(pos_en_atril).Update(disabled=True)
                     window.FindElement('-LetterSelected-').Update('')
                 else: 
                     continue
             elif cant_letras >= 2:
                 #Si ya se ingresaron 2 o mas letras
-                letra_ant = letter
-                palabra += letter
-                letter_selected = False
-                tuple_list.append(event)
                 if event == next_button:
+                    letra_ant = letter
+                    palabra += letter
+                    letter_selected = False
+                    tuple_list.append(event)
                     next_button = ingresa_tercera(window,event,escritura,letter,next_button)
                     cant_letras += 1
+                    window.FindElement(pos_en_atril).Update(disabled=True)
+                    window.FindElement('-LetterSelected-').Update('')
                 else:
                     continue
             elif event == (14,14):
@@ -259,8 +264,8 @@ def play_player (player, window):
                     window.FindElement('-SCORE-').Update(player.get_puntaje())
                     window.FindElement('-MESSAGE-').Update('palabra correcta')
                     window.FindElement(next_button).Update(button_color = ('white','blue'))
-                    block_word(window,tuple_list,palabra)
-                    fill_letters(player,window)
+                    block_word(window,tuple_list,palabra,board)
+                    fill_letters(player,window,admin)
                     break
                 else:
                     window.FindElement('-MESSAGE-').Update(str(palabra)+' no es una palabra')
@@ -272,7 +277,7 @@ def play_player (player, window):
             if cant_letras == 0:
                 window.FindElement('-MESSAGE-').Update('No Hay Letras Ingresadas En Este Turno')
             else:
-                vuelta_atras(window,player,palabra[len(palabra)-1],tuple_list[len(tuple_list)-1],cant_letras,escritura)
+                vuelta_atras(window,player,board,palabra[len(palabra)-1],tuple_list[len(tuple_list)-1],cant_letras,escritura)
                 tuple_list.pop()
                 palabra = palabra[0:-1]
                 cant_letras -= 1
@@ -281,23 +286,26 @@ def play_player (player, window):
     return event
 
 
-def game_procces (window,player,compu):
+def game_procces (window,admin,board,player,compu):
     rand_start = rand(1,2)
     if (rand_start == 1):
         while True:
-            event = play_player (player,window)
+            event = play_player(player,window,admin,board)
             #compu.jugar (compu,window)
             if event in (None, '-mainMenu-'):
                 break
     else:
         while True:
             #compu.jugar (compu,window)
-            event = play_player (player,window)
+            event = play_player (player,window,admin,board)
             if event in (None, '-mainMenu-'):
                 break
     return event
 
 def main(configs):
+    admin = AdministradorDeJuego(configs['dificultad'])
+    lista_tuplas = admin.devolver_tuplas()
+    board = Tablero(lista_tuplas)           
     window = sg.Window('ScrabbleAR', board.set_layout(configs),background_color=('#1CB7C3'))
     while True:
         event,_ = window.read()
@@ -310,8 +318,8 @@ def main(configs):
             board.set_time_left(tiempo_restante)
             player = jugador(configs['name'],admin.tomar_fichas(7))
             compu = jugador('CPU',admin.tomar_fichas(7))
-            game_on(window,configs,player.get_fichas())
-            event = game_procces(window,player,compu)
+            game_on(window,admin,board,configs,player.get_fichas())
+            event = game_procces(window,admin,board,player,compu)
         if event in (None, '-mainMenu-'):
             break
         else:
