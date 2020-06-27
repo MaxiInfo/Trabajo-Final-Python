fc3549634942c124345d67edc0fbffd4d73ab82f
tensa2205 <62038818+tensa2205@users.noreply.github.com>
2020-06-27 07:31:27 -0300

    Actualizar top y layout puntajes
    ->Agregado .py para actualizar el archivo de TOP10 en base a una dificultad dada.
    ->Agregada una layout para modificar los puntajes de cada letra.

---
+++ archivosPy/Actualizar_top.py
@@ -0,0 +1,43 @@
+import json
+
+def actualizar(nombre_jugador,puntaje,fecha,dificultad_jugada):
+    '''El siguiente modulo lo que hace es actualizar el top10 de una dificultad previamente jugada.
+        Abarca todos los casos posibles:
+            Sí repito el nombre como jugador y supero el puntaje existente -> lo actualizo.
+            Sí repito el nombre como jugador PERO no supero el puntaje existente -> no se actualiza.
+            Sí es un nuevo nombre, puede que entre o no al top10, de todas formas sí entra se verá bién posicionado, caso contrario 
+                no ingresa.
+    '''
+
+    nombre = 'archivoTop.json'
+    sigo = True
+    try:
+        with open(nombre,'r') as archivo_top:
+            diccionario_completo = json.load(archivo_top) #Tomo todo el diccionario
+            diccionario_ordenar = diccionario_completo[dificultad_jugada] #facil, medio, o dificil.
+
+            if nombre_jugador in diccionario_ordenar: #Sí existe mi jugador. 
+                if puntaje > diccionario_ordenar[nombre_jugador][0]: #Sí existe mi jugador y el puntaje que tiene es mayor al original.
+                    diccionario_ordenar[nombre_jugador] = [puntaje,fecha]
+                else:
+                    sigo = False
+                #El hecho de usar dos if's, es que si uso AND se puede dar el caso que -> existe el jugador, pero no supera el puntaje.
+                    #Por lo tanto iría al else y así y todo actualizaría con el puntaje menor, DEJENLO COMO ESTÁ.
+            else:
+                diccionario_ordenar[nombre_jugador] = [puntaje,fecha] #Inserto
+            if sigo:
+                lista = sorted(diccionario_ordenar.items(), key = lambda item : item[1][0],reverse = True)
+                if len(lista) == 11:
+                    del lista[10]
+                    
+                diccionario_actualizado = {elemento[0]:[elemento[1][0],elemento[1][1]] for elemento in lista}
+                diccionario_completo[dificultad_jugada] = diccionario_actualizado
+                json.dump(diccionario_completo,archivo_top) #Guardo el diccionario actualizado.
+            archivo_top.close()
+
+    except FileNotFoundError:
+        #Si se dá el caso que el usuario borra el archivoTop.json por que es un cliente desastroso, no estaría mal generarlo de nuevo.
+        diccionario = {"facil": {"AAA": [50, "22/5"], "BBB": [45, "22/5"], "CCC": [40, "22/5"], "DDD": [35, "22/5"], "EEE": [30, "22/5"], "FFF": [25, "22/5"], "GGG": [20, "22/5"], "HHH": [15, "22/5"], "III": [10, "22/5"], "JJJ": [5, "22/5"]}, "medio": {"KKK": [70, "23/5"], "LLL": [65, "23/5"], "MMM": [60, "23/5"], "NNN": [55, "23/5"], "OOO": [50, "23/5"], "PPP": [45, "23/5"], "QQQ": [40, "23/5"], "RRR": [35, "23/5"], "SSS": [30, "23/5"], "TTT": [25, "23/5"]}, "dificil": {"j1": [45, "24/5"], "j2": [40, "24/5"], "j3": [35, "24/5"], "j4": [30, "24/5"], "j5": [25, "24/5"], "j6": [20, "24/5"], "j7": [15, "24/5"], "j8": [10, "24/5"], "j9": [5, "24/5"], "j10": [0, "24/5"]}}
+        archivo_nuevo = open(nombre,'w')
+        json.dump(diccionario,archivo_nuevo)
+        archivo_nuevo.close()
---
+++ archivosPy/Puntajes.py
@@ -0,0 +1,74 @@
+import PySimpleGUI as sg
+def set_layout():
+    '''
+        Paso a explicar, tupla contiene los valores que voy a poder seleccionar en mi inputcombo.
+            InputCombo -> tupla -> valor por defecto siempre es 5. La llave permite que mi diccionario values tenga dicha clave.
+            Readonly, sirve para que la casilla del inputcombo NO SEA ESCRITA, por lo tanto, sólo permite que elijas entre 5-15.
+            El método devuelve la Layout ya armada
+    '''
+    tupla = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+    col1 = [
+        [sg.Text('A',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[12],size=(3,1),key='A',readonly=True)],
+        [sg.Text('B',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[4],size=(3,1),key='B',readonly=True)],
+        [sg.Text('C',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[4],size=(3,1),key='C',readonly=True)],
+        [sg.Text('D',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[5],size=(3,1),key='D',readonly=True)],
+        [sg.Text('E',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[12],size=(3,1),key='E',readonly=True)],
+        [sg.Text('F',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[3],size=(3,1),key='F',readonly=True)]
+    ]
+    col2 = [
+        [sg.Text('G',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[3],size=(3,1),key='G',readonly=True)],
+        [sg.Text('H',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[2],size=(3,1),key='H',readonly=True)],
+        [sg.Text('I',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[6],size=(3,1),key='I',readonly=True)],
+        [sg.Text('J',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[3],size=(3,1),key='J',readonly=True)],
+        [sg.Text('K',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[3],size=(3,1),key='K',readonly=True)],
+        [sg.Text('L',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[4],size=(3,1),key='L',readonly=True)]
+    ]
+    col3 = [
+        [sg.Text('LL',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[2],size=(3,1),key='LL',readonly=True)],
+        [sg.Text('M',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[3],size=(3,1),key='M',readonly=True)],
+        [sg.Text('N',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[5],size=(3,1),key='N',readonly=True)],
+        [sg.Text('Ñ',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[2],size=(3,1),key='Ñ',readonly=True)],
+        [sg.Text('O',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[9],size=(3,1),key='O',readonly=True)],
+        [sg.Text('P',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[3],size=(3,1),key='P',readonly=True)]
+    ]
+    col4 = [
+        [sg.Text('Q',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[2],size=(3,1),key='Q',readonly=True)],
+        [sg.Text('R',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[5],size=(3,1),key='R',readonly=True)],
+        [sg.Text('RR',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[1],size=(3,1),key='RR',readonly=True)],
+        [sg.Text('S',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[6],size=(3,1),key='S',readonly=True)],
+        [sg.Text('T',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[4],size=(3,1),key='T',readonly=True)],
+        [sg.Text('U',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[5],size=(3,1),key='U',readonly=True)]
+    ]
+    col5 = [
+        [sg.Text('V',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[2],size=(3,1),key='V',readonly=True)],
+        [sg.Text('W',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[1],size=(3,1),key='W',readonly=True)],
+        [sg.Text('X',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[2],size=(3,1),key='X',readonly=True)],
+        [sg.Text('Y',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[3],size=(3,1),key='Y',readonly=True)],
+        [sg.Text('Z',size=(2,1),justification='center'),sg.InputCombo(tupla,default_value=tupla[2],size=(3,1),key='Z',readonly=True)]
+    ]
+
+    layout = [
+        [sg.Text('Configuración puntaje de las fichas',size=(38,1),font=(None,15),justification='center',background_color='purple')],
+        [sg.Text('')],
+        [sg.Column(col1,background_color='black'),sg.Column(col2,background_color='red'),sg.Column(col3,background_color='green'),sg.Column(col4,background_color='blue'),sg.Column(col5,background_color='white')],
+        [sg.Button('Guardar',size=(25,1)),sg.Button('Atrás',size=(25,1))]
+    ]
+    
+    return layout
+
+def main():
+    '''
+        Sí cambio es True, es por que el usuario tocó en guardar, por lo tanto quiere usar la bolsa que modificó -> la devuelvo
+        Sí cambio es False, es por que el usuario tocó Atrás, por lo tanto NO quiere usar la bolsa que modificó -> no la devuelvo
+    '''
+    window = sg.Window('Puntaje de las fichas',set_layout())
+    while True:
+        event,values = window.read()
+        window.close()
+        if event in (None,'Atrás'):
+            return None
+        if event in ('Guardar'):
+            return values
+
+#bolsa = configuracion_bolsa()
+#print(bolsa)
